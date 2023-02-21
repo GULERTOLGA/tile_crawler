bcfaeea68b56c63b69d75b2ea9bab5f1c00a97c2
Azad Erdoğan <azad.erdogan@netcad.com>
2023-02-21 12:25:13 +0300

    concurrency added

--- lib/tile_crawler.dart
+++ lib/tile_crawler.dart
@@ -1,7 +1,9 @@
 library tile_crawler;
 
 import 'dart:async';
+import 'dart:developer' as dev;
 import 'dart:io';
+import 'dart:isolate';
 import 'dart:math';
 
 //https://mt1.google.com/vt/lyrs=m&x={x}&y={y}&z={z}
@@ -12,12 +14,14 @@ typedef OnStart = void Function(int totalTileCount, double area);
 
 typedef OnProcess = void Function(int tileDownloaded, int z, int x, int y);
 typedef OnEnd = void Function();
+typedef OnComplete = void Function(bool success, _XYZ xyz);
 
 class TileCrawler {
   final int tileSize = 256;
   final DownloadOptions options;
   final List<_XYZ> _queue = [];
-
+  static int _completedIsolateCount = 0;
+  static int installedTileCount = 0;
   TileCrawler(this.options);
 
   bool _cancel = false;
@@ -29,17 +33,23 @@ class TileCrawler {
     for (int z = options.minZoomLevel; z <= options.maxZoomLevel; z++) {
       _calculateRectIN(_calculateRect(options.topLeft, options.bottomRight, z));
     }
-    if (onStart != null) {
-      onStart(_queue.length, 1500.0);
-    }
+
     var startCount = _queue.length;
-    while (!_cancel && _queue.isNotEmpty) {
-      var xyz = _queue.removeLast();
-      await _downloadCurrent(xyz);
-      if (onProcess != null) {
-        onProcess(startCount - _queue.length, xyz.z, xyz.x, xyz.y);
-      }
-    }
+    installedTileCount = startCount;
+    downloadWithIsolate(onStart, onProcess, onEnd);
+    /*  while (!_cancel && _queue.isNotEmpty) {
+      var xyzTemp = _queue.removeLast();
+      _downloadCurrent(xyzTemp)
+          .then((xyz) => {
+                if (onProcess != null)
+                  {onProcess(startCount - _queue.length, xyz.z, xyz.x, xyz.y)}
+              })
+          .whenComplete(() {
+        if (_queue.isEmpty && onEnd != null) {
+          onEnd();
+        }
+      });
+    } */
   }
 
   void cancel() {
@@ -85,7 +95,18 @@ class TileCrawler {
     }
   }
 
-  Future<void> _downloadCurrent(_XYZ current) async {
+  Future<_XYZ> _downloadCurrent(_XYZ current) async {
+    /* return await downloadWithIsolate(url, current).then((value) => {
+          //   if (value == false) {downloadWithoutIsolate(url, current)}
+          return value;
+        })  */
+    // await downloadWithIsolate(current);
+    //await downloadWithoutIsolate(url, current);
+
+    return current;
+  }
+
+  Future<void> downloadWithoutIsolate(_XYZ current) async {
     var url = options.tileUrlFormat.toLowerCase();
     if (url.contains("{quadkey}")) {
       url = url.replaceAll("{quadkey}", current.toQuadKey());
@@ -95,13 +116,98 @@ class TileCrawler {
           .replaceAll("{y}", current.y.toString())
           .replaceAll("{z}", current.z.toString());
     }
-
     final request = await options.client.getUrl(Uri.parse(url));
     final response = await request.close();
     final String pathString =
         "${options.downloadFolder}/${current.z}/${current.x}";
     final dirPath = await Directory(pathString).create(recursive: true);
     await response.pipe(File('${dirPath.path}/${current.y}.png').openWrite());
+  }
+
+  Future<bool?> downloadWithIsolate(
+      OnStart? onStart, OnProcess? onProcess, OnEnd? onEnd) async {
+    int concurrent = 10;
+    int remainingValue = _queue.length % concurrent;
+    int divisiorValue = (_queue.length - concurrent) ~/ concurrent;
+    List parsedList = [];
+    for (int i = 0; i < concurrent; i++) {
+      if (i == concurrent - 1) {
+        parsedList.add(_queue.sublist(
+            i * divisiorValue, i * divisiorValue + remainingValue));
+      } else {
+        parsedList.add(_queue.sublist(
+            i * divisiorValue, i * divisiorValue + divisiorValue));
+      }
+    }
+    for (var i = 0; i < concurrent; i++) {
+      final receivePort = ReceivePort();
+      Isolate.spawn(_download, {
+        'sendPort': receivePort.sendPort,
+        'xyzList': parsedList[i],
+        'client': options.client,
+      });
+      receivePort.listen((message) {
+        if (message == "complete") {
+          _completedIsolateCount++;
+          dev.log("$_completedIsolateCount", name: "TileCrawler:completed");
+          if (_completedIsolateCount == concurrent) {
+            if (onEnd != null) {
+              onEnd();
+            }
+          }
+        } else if (message == "start") {
+          if (onStart != null) {
+            onStart(_queue.length, 1500.0);
+          }
+        } else if (message == "error") {
+          if (onEnd != null) {
+            onEnd();
+          }
+        } else if (message is _XYZ) {
+          installedTileCount--;
+          if (onProcess != null) {
+            onProcess(_queue.length - installedTileCount, message.z, message.x,
+                message.y);
+          }
+        }
+      });
+    }
+  }
+
+  void _download(Map<String, dynamic> message) async {
+    final xyzList = message['xyzList'] as List<_XYZ>;
+    final sendPort = message['sendPort'] as SendPort;
+    final client = message['client'] as HttpClient;
+    try {
+      sendPort.send("start");
+      while (!_cancel && xyzList.isNotEmpty) {
+        var current = xyzList.removeLast();
+        var url = options.tileUrlFormat.toLowerCase();
+        if (url.contains("{quadkey}")) {
+          url = url.replaceAll("{quadkey}", current.toQuadKey());
+        } else {
+          url = url
+              .replaceAll("{x}", current.x.toString())
+              .replaceAll("{y}", current.y.toString())
+              .replaceAll("{z}", current.z.toString());
+        }
+        dev.log(url.toString());
+        final request = await client.getUrl(Uri.parse(url));
+        final response = await request.close();
+        final String pathString =
+            "${options.downloadFolder}/${current.z}/${current.x}";
+        final dirPath = await Directory(pathString).create(recursive: true);
+        await response
+            .pipe(File('${dirPath.path}/${current.y}.png').openWrite());
+        sendPort.send(current);
+        dev.log("${current.z} ${current.x} ${current.y}",
+            name: "TileCrawler:downloaded", level: 1000);
+      }
+      sendPort.send("complete");
+    } catch (e) {
+      dev.log(e.toString(), name: "error", level: 1000, error: e);
+      sendPort.send(e);
+    }
   }
 }
 
